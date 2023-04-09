56815b28e8a033b3af693a901235a07b38d63832
Semen Saifutdinov <saifutdinov2006@mail.ru>
2023-04-09 17:33:50 +0300

    Added productsapp and modified base.html

--- shope/shope/settings.py
+++ shope/shope/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shope.authapp.apps.AuthappConfig',
+    'shope.productsapp.apps.ProductsappConfig'
 ]
 
 MIDDLEWARE = [
