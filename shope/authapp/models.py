8b42264a771a4858caa120d98c764179609ee518
arsen_iamaleev <iiforex@mail.ru>
2023-04-16 22:43:17 +0300

    change urls, templates name and add user_activation_key in User

--- shope/authapp/models.py
+++ shope/authapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import datetime, timedelta
 
 
 class User(AbstractUser):
@@ -8,12 +9,26 @@ class User(AbstractUser):
     """
 
     middle_name = models.CharField("Отчество", max_length=150, blank=True)
-    avatar = models.ImageField("Аватар", upload_to="profiles")
     email = models.EmailField("Адрес электронной почты", unique=True)  # переопределение
     # email с целью сделать это поле уникальным
+    activation_key = models.CharField(max_length=50, blank=True)  # ключ для активации аккаунта
+    activation_key_expires = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = ["first_name",
+                       "last_name",
+                       ]
+
+    def is_activation_key_expires(self):
+        """
+        Метод, который проверяет, попадает ли ввод ключа
+        в установленный временной интервал
+        return: Bool
+        """
+
+        if datetime.now() <= self.activation_key_expires + timedelta(hours=72):
+            return False
+        return True
 
     def __str__(self):
         return self.username
--- shope/authapp/urls.py
+++ shope/authapp/urls.py
@@ -5,12 +5,12 @@ from django.views.generic import TemplateView
 app_name = 'authapp'
 
 urlpatterns = [
-    path('mail/', TemplateView.as_view(template_name=
-                                       "authapp/mail.html")),
+    path('resetpassword/mail/', TemplateView.as_view(template_name=
+                                       "authapp/email_to_change_password.html")),
     path('login/', TemplateView.as_view(template_name=
                                         "authapp/login.html")),
-    path('password/', TemplateView.as_view(template_name=
-                                           "authapp/password.html")),
+    path('resetpassword/newpassword/', TemplateView.as_view(template_name=
+                                           "authapp/change_password.html")),
     path('registr/', TemplateView.as_view(template_name=
                                           "authapp/registr.html")),
 
