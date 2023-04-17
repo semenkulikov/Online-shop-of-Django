f2028a340ad03fa41cdf0777a28f426b833f58bc
Артём Пашинский <artpash2005@gmail.com>
2023-04-17 23:01:47 +0300

    структура URL для order

--- shope/orderapp/urls.py
+++ shope/orderapp/urls.py
@@ -5,7 +5,9 @@ from django.views.generic import TemplateView
 app_name = 'orderapp'
 
 urlpatterns = [
+
     path('basket/', TemplateView.as_view(template_name="orderapp/basket.html")),
     path('checkout/', TemplateView.as_view(template_name="orderapp/checkout.html")),
     path('payment/', TemplateView.as_view(template_name="paymentapp/payment.html")),
+
 ]
