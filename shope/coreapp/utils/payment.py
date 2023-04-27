8618b6b1111595c275469c3d7d7a26fad90019f6
Alexander Barshev <drivetime@bk.ru>
2023-04-27 11:18:42 +0300

    utils methods added

--- shope/coreapp/utils/payment.py
+++ shope/coreapp/utils/payment.py
@@ -1,3 +1,15 @@
 class Payment:
-    """Оплата"""
-    pass
+    """
+    Сервис работы с платежами через ЮKassa
+    """
+    def get_payment_status(self):
+        """
+        Проверка статуса платежа
+        """
+        pass
+
+    def create_payment(self):
+        """
+        Создание и отправка нового платежа
+        """
+        pass
