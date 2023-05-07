3767b164623f62ecfd581bb15cdd53a213744167
Sergei Nozovtsev <glenmetthews@gmail.com>
2023-05-07 18:54:43 +0300

    added profile info

--- shope/profileapp/views.py
+++ shope/profileapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Profile
+
+
+class ProfileDetailView(DetailView, LoginRequiredMixin):
+    queryset = Profile.objects.all()
+    template_name = 'profileapp/account.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user.profile
