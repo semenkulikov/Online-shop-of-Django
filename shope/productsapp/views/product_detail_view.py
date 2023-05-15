03ab2f81b45ae32f7d4717d5de04d72431e3c668
Semen Saifutdinov <saifutdinov2006@mail.ru>
2023-05-15 14:29:44 +0300

    Fix fixtures and views

--- shope/productsapp/views/product_detail_view.py
+++ shope/productsapp/views/product_detail_view.py
@@ -1,18 +1,16 @@
-from django.http import HttpRequest, HttpResponse
+﻿from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
 from productsapp.models import Product
-from productsapp.forms import AddReviewForm
 from coreapp.utils.add_product_review import AddProductReview
 from repositories.profile_repository import ProfileRepository
 from repositories import SellerSelectRepository, SpecificSelectRepository
 
 
-class AddReviewView(View):
+class ProductDetailView(View):
     """
-    Класс-view для добавления отзыва к продукту
+    Класс-view для отображения детальной страницы продукта
     """
-    form_class = AddReviewForm
     _service = AddProductReview()
     _profile_repository = ProfileRepository()
     select_seller_repo = SellerSelectRepository()
@@ -33,8 +31,7 @@ class AddReviewView(View):
         )
 
         return render(request, "productsapp/product.html",
-                      context={"form": self.form_class,
-                               "product": product,
+                      context={"product": product,
                                "amount_review": amount_review,
                                "reviews_list": reviews_list,
                                'sellers': sellers,
@@ -42,23 +39,10 @@ class AddReviewView(View):
                                "user": request.user})
 
     def post(self, request: HttpRequest, product_id: int) -> HttpResponse:
-        form = self.form_class(data=request.POST)  # форма с отзывом
         product = Product.objects.get(id=product_id)
         amount_review = self._service.product_reviews_amount(product=product)
         reviews_list = self._service.product_reviews_list(product=product)
-        if form.is_valid():
-            # Если форма валидна, берем отзыв и добавляем к продукту
-            text = form.cleaned_data.get("text")
-            result = self._service.add_product_review(
-                user=self._profile_repository.get_profile(request.user),
-                product=product,
-                text=text
-            )
-        else:
-            result = "Введены некорректные данные!"
         return render(request, "productsapp/product.html",
-                      context={"form": self.form_class,
-                               "product": product,
-                               "result": result,
+                      context={"product": product,
                                "amount_review": amount_review,
                                "reviews_list": reviews_list})
