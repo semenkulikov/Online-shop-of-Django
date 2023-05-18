bd117c1f86604b9a4f65d32b178e77e7cd75bcbf
arsen_iamaleev <iiforex@mail.ru>
2023-05-18 15:55:55 +0300

    add templates-email messages

--- shope/coreapp/utils/verified_user.py
+++ shope/coreapp/utils/verified_user.py
@@ -47,24 +47,3 @@ def generate_random_string():
     rand_string = ''.join(secrets.choice(
         letters_and_digits) for i in range(13))
     return rand_string
-
-# def send_verif_link(user):
-#     """
-#     Метод создания и отправки сообщения на e-mail
-#     :return: send_mail
-#     :rtype: bool
-#     """
-#     verif_link = reverse('authapp:verified',
-#                          kwargs={'email': user.email,
-#                                  'key': user.activation_key
-#                                  }
-#                          )  # ссылка для активации
-#     subject = 'Активация аккаунта'  # тема
-#     message = f'Для подтверждения электронной почты' \
-#               f' {user.email} на портале \n ' \
-#               f'{settings.DOMAIN_NAME} пройдите по ссылке \n' \
-#               f'{settings.DOMAIN_NAME}{verif_link}'
-#     # сообщение
-#     return send_mail(subject, message,
-#                      settings.EMAIL_HOST_USER,
-#                      [user.email])  # отправка email
