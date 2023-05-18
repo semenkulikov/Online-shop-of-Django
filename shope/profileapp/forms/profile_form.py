70209063899008ec381862f632f8720eea93e8a7
Sergei Nozovtsev <glenmetthews@gmail.com>
2023-05-19 01:42:52 +0300

    fixed unique field for phone_number

--- shope/profileapp/forms/profile_form.py
+++ shope/profileapp/forms/profile_form.py
@@ -36,6 +36,13 @@ class ProfileForm(forms.ModelForm):
 
     def clean_avatar_image(self):
         data = self.cleaned_data["avatar_image"]
-        if data.size > settings.MAX_AVATAR_IMAGE_SIZE:
-            raise ValidationError("Maximum avatar size is 2 Mb")
+        if data:
+            if data.size > settings.MAX_AVATAR_IMAGE_SIZE:
+                raise ValidationError("Maximum avatar size is 2 Mb")
         return data
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data['phone_number']
+        if profile_rep.get_profile_by_phone_number(phone_number=phone_number):
+            raise ValidationError('This phone number is already in use')
+        return phone_number
