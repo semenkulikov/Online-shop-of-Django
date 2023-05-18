2a0d62bf255b7677c92322d1dfda5cc3940f2516
Артём Пашинский <artpash2005@gmail.com>
2023-05-18 18:26:13 +0300

    celery add

--- shope/coreapp/views.py
+++ shope/coreapp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from shope.shope.tasks import send_order_confirmation_email
+from django.http import HttpResponse
+from shope.shope.tasks import process_image
+
+
+def create_order(request):
+    # Логика создания заказа
+    # ...
+
+    # Отправка задачи Celery для отправки письма с подтверждением заказа
+    send_order_confirmation_email.delay(order.id)
+
+    # Ответ пользователю
+    return HttpResponse('Заказ успешно оформлен.')
+
+
+def process_image_view(request):
+    # Логика получения пути к изображению
+    image_path = '/path/to/image.jpg'
+
+    # Отправка задачи Celery для обработки изображения
+    result = process_image.delay(image_path)
+
+    # Получение результата обработки изображения (необязательно)
+    # processed_image_path = result.get()
+
+    # Ответ пользователю
+    return HttpResponse('Изображение отправлено на обработку.')
+
--- shope/orderapp/views.py
+++ shope/orderapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Order
 
-# Create your views here
+
+def order_history(request):
+    orders = Order.objects.filter(user=request.user).order_by('-order_date')
+    return render(request, 'historyorder.html', {'orders': orders})
 
