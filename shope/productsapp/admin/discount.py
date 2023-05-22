03c11396c9a2a6d219670c4a6bbc88d89bf0896c
Alexander Barshev <drivetime@bk.ru>
2023-05-23 00:35:55 +0300

    add discount models (with inlines in admin)

--- shope/productsapp/admin/discount.py
+++ shope/productsapp/admin/discount.py
@@ -1,4 +1,44 @@
 from django.contrib import admin
-from productsapp.models.discount import Discount
+from productsapp.models.discount import Discount, \
+    ProductDiscount, SetDiscount, CartDiscount
 
 admin.site.register(Discount)
+
+
+class ProductInline(admin.StackedInline):
+    model = ProductDiscount.products.through
+    extra = 0
+
+
+class CategoryInline(admin.StackedInline):
+    model = ProductDiscount.categories.through
+    extra = 0
+
+
+class ProductSetInline(admin.StackedInline):
+    model = SetDiscount.products.through
+    extra = 0
+
+
+@admin.register(ProductDiscount)
+class AdminProductDiscount(admin.ModelAdmin):
+    inlines = [
+        ProductInline, CategoryInline
+    ]
+    list_display = ['name', 'value', 'start_date',
+                    'expiration_date', 'priority']
+
+
+@admin.register(SetDiscount)
+class AdminSetDiscount(admin.ModelAdmin):
+    inlines = [
+        ProductSetInline,
+    ]
+    list_display = ['name', 'value', 'start_date',
+                    'expiration_date', 'priority']
+
+
+@admin.register(CartDiscount)
+class AdminCartDiscount(admin.ModelAdmin):
+    list_display = ['name', 'value', 'start_date', 'expiration_date',
+                    'priority', 'required_sum', 'required_quantity']
--- shope/productsapp/models/discount.py
+++ shope/productsapp/models/discount.py
@@ -1,6 +1,6 @@
 from django.db import models
 from coreapp.models import BaseModel
-from productsapp.models.product import Product
+from productsapp.models.product import Product, Category
 
 
 class Discount(BaseModel):
@@ -24,3 +24,72 @@ class Discount(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class BaseDiscount(BaseModel):
+    """ Базовый абстрактный класс модели скидок """
+    name = models.CharField(
+        max_length=50,
+        blank=True,
+        null=False,
+        verbose_name='name')
+    value = models.DecimalField(
+        null=True,
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='value')
+    start_date = models.DateField(
+        null=False,
+        blank=True,
+        verbose_name='start date')
+    expiration_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='expiration date')
+    description = models.CharField(
+        max_length=200,
+        blank=True,
+        null=False,
+        verbose_name='description')
+    priority = models.PositiveSmallIntegerField(
+        null=False,
+        blank=False,
+        verbose_name='priority')
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f'{self.name} - {self.value} - {self.priority}'
+
+
+class SetDiscount(BaseDiscount):
+    """ Класс-модель скидки на фиксированный набор продуктов """
+    products = models.ManyToManyField(
+        Product,
+        related_name='set_discounts',
+        verbose_name='products')
+
+
+class ProductDiscount(BaseDiscount):
+    """ Класс-модель скидки для списка продуктов и категорий"""
+    products = models.ManyToManyField(
+        Product,
+        related_name='product_discounts',
+        verbose_name='products')
+    categories = models.ManyToManyField(
+        Category,
+        related_name='category_discounts')
+
+
+class CartDiscount(BaseDiscount):
+    """Класс-модель скидки на корзину товаров"""
+    required_sum = models.DecimalField(
+        null=True,
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='required sum')
+    required_quantity = models.PositiveSmallIntegerField(
+        null=False,
+        blank=True,
+        verbose_name='required quantity')
