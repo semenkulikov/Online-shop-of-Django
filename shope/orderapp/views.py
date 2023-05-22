43483eb9ad649cf02e156e5ac7c04247d4395d4f
Артём Пашинский <artpash2005@gmail.com>
2023-05-22 13:04:29 +0300

    delete views

--- shope/orderapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Order
-
-
-def order_history(request):
-    orders = Order.objects.filter(user=request.user).order_by('-order_date')
-    return render(request, 'historyorder.html', {'orders': orders})
-
