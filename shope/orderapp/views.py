f12a30b07631609992d602e6dc200eba8f5f1599
Alexander Barshev <drivetime@bk.ru>
2023-05-26 01:09:22 +0300

    fix order steps

--- shope/orderapp/views.py
+++ shope/orderapp/views.py
@@ -1,26 +1,17 @@
 from django.views.generic import View
-from profileapp.forms import ProfileForm, UserForm, UserPasswordSetForm
+from profileapp.forms import ProfileForm
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
-class OrderView(View):
+class OrderView(LoginRequiredMixin, View):
     template_name = 'orderapp/order.html'
+    login_url = reverse_lazy('authapp:login')
 
     def get(self, request):
-        if request.user.is_authenticated:
-            context = {
-                'user_form': UserForm(
-                    instance=self.request.user),
-                'profile_form': ProfileForm(
-                    instance=self.request.user.profile),
-                'password_form': UserPasswordSetForm(
-                    user=self.request.user)
-            }
-        else:
-            context = {
-                'user_form': UserForm(),
-                'profile_form': ProfileForm(),
-                'password_form': UserPasswordSetForm(
-                    user=self.request.user)
-            }
+        context = {
+            'profile_form': ProfileForm(
+                instance=self.request.user.profile),
+        }
         return render(request, self.template_name, context)
