940e55f56fef27d7461c44b6ca623c93ed259c8c
Sergei Nozovtsev <glenmetthews@gmail.com>
2023-05-26 04:10:21 +0300

    added orders

--- shope/repositories/order_select_repository.py
+++ shope/repositories/order_select_repository.py
@@ -1,16 +1,25 @@
-from django.db.models import QuerySet
 from interfaces import OrderInterface
 from orderapp.models import Order
 from authapp.models import User
+from django.db.models import QuerySet, Sum
 
 
 class OrderRepository(OrderInterface):
 
     def get_all(self) -> QuerySet[Order]:
-        return Order.objects.all()
+        return Order.objects.annotate(
+            sum_price=Sum('order_items__price'),
+        )
 
     def get_last_activ(self, user: User) -> QuerySet[Order]:
-        return Order.objects.filter(
+        return Order.objects.annotate(
+            sum_price=Sum('order_items__price')
+        ).filter(
             user=user,
             is_active=True,
             ).order_by('created_at').last()
+
+    def get_order_by_id(self, order_id: int):
+        return Order.objects.annotate(
+            amount=Sum('order_items__price')
+        ).get(id=order_id)
