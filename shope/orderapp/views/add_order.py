64bb0489c3b0395dafe88105799fa52dd61b5add
Alexander Barshev <drivetime@bk.ru>
2023-05-30 01:08:54 +0300

    order v.2

--- shope/orderapp/views/add_order.py
+++ shope/orderapp/views/add_order.py
@@ -4,6 +4,14 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy, reverse
 from django.http import HttpResponseRedirect
+from orderapp.forms import OrderForm
+from coreapp.utils.select_cart import SelectCart
+from orderapp.models import Order, OrderItem
+from repositories.price_repository import PriceRepository
+from repositories.cart_repository import RepCartItem
+
+rep_price = PriceRepository()
+rep_cartitem = RepCartItem()
 
 
 class AddOrderView(LoginRequiredMixin, View):
@@ -11,9 +19,28 @@ class AddOrderView(LoginRequiredMixin, View):
     login_url = reverse_lazy('authapp:login')
 
     def get(self, request):
+        cart_items = SelectCart.cart_items_list(user=request.user)
+        print(cart_items)
+        order = Order(user=request.user, status='not paid')
+        order.save()
+        for item in cart_items:
+            order_item = OrderItem(
+                order=order,
+                product=item.product,
+                seller=item.seller,
+                count=item.quantity,
+                price=rep_price.get_price(
+                    item.product, item.seller) * item.quantity)
+            order_item.save()
+
+        rep_cartitem.delete(cart_items)
+        profile_form = ProfileForm(instance=self.request.user.profile)
+        for name, field in profile_form.fields.items():
+            field.widget.attrs['id'] = name
+
         context = {
-            'profile_form': ProfileForm(
-                instance=self.request.user.profile),
+            'profile_form': profile_form,
+            'order_form': OrderForm()
         }
         return render(request, self.template_name, context)
 
