2f427d3ed805f3ca3729e99054d7c66d1c059d56
arsen_iamaleev <iiforex@mail.ru>
2023-06-03 22:40:00 +0300

    django utils change -> fixed session operations in cart(add, delete, remove, move)
    Took 2 minutes

--- shope/coreapp/utils/select_cart.py
+++ shope/coreapp/utils/select_cart.py
@@ -3,8 +3,6 @@ from repositories.seller_select_repository import SellerSelectRepository
 from repositories.product_select_repository import ProductSelectRepository
 from repositories.price_repository import PriceRepository
 
-from cartapp.models import Cart
-
 rep_cart = RepCart()
 rep_cart_item = RepCartItem()
 rep_seller = SellerSelectRepository()
@@ -24,7 +22,8 @@ class SelectCart:
         Если пользователь авторизован, то методу
         передаётся экземпляр пользователя
         Если не авторизован, то передаётся словарь:
-        {'products': {'product_id': [count, seller_id]...}
+        session_products =
+        {'product_id seller_id': count,...}
         """
         items_list = []
         if user:  # пользователь авторизован
@@ -33,27 +32,64 @@ class SelectCart:
             items_list = rep_cart_item.get_all_items(cart)
             # список товаров в корзине
         else:  # есть товары в сессии
-            for product_id in session_products:
-                # цикл по ключам "product_id" в сессии
-                product = rep_prod.\
+            for item in session_products:
+                # цикл по ключам "product_id" и "seller_id" в сессии
+                product_id, seller_id = item.split()[0], item.split()[1]
+                product = rep_prod. \
                     get_product_by_id(product_id)  # продукт
-                seller = rep_seller.\
-                    get_seller(session_products[product_id][1])  # продавец
+                seller = rep_seller. \
+                    get_seller(seller_id)  # продавец
                 items_list.append(rep_price.get_object_price(product, seller))
         return items_list
 
     @classmethod
-    def cart_items_amount(cls, cart: Cart):
+    def cart_items_amount(cls, user=None, session_products=None):
         """
         Получение общего количества товаров в корзине
+        если есть товары в сессии, то в метод передаётся словарь
+        session_products =
+        {'product_id seller_id': count,...}
         """
-        count = rep_cart.count_items(cart)
+        if user:
+            # в метод передан экземпляр пользователя(авторизован)
+            cart = rep_cart.get_cart(user)
+            count = rep_cart.count_items(cart)
+        elif session_products:
+            # в метод передан словарь <session_products>
+            count = sum([count for count in
+                         session_products.values()])
+        else:
+            count = 0
         return count
 
     @classmethod
-    def cart_total_amount(cls, cart: Cart):
+    def cart_total_amount(cls, user=None, session_products=None):
         """
-        Получение суммы цен всех товаров в корзине
+        Получение общей цены товаров в корзине
+        если есть товары в сессии, то в метод передаётся словарь
+        session_products =
+        {'product_id seller_id': count,...}
         """
-        total_amount = rep_cart.get_total_amount(cart)
+        if user:
+            # в метод передан экземпляр пользователя(авторизован)
+            cart = rep_cart.get_cart(user)
+            total_amount = rep_cart.get_total_amount(cart)
+        elif session_products:
+            # в метод передан словарь <session_products>
+            cart_sum = []
+            for item in session_products:
+                product_id, seller_id = item.split()[0], item.split()[1]
+                cart_sum.append(
+                    rep_price.
+                    get_price(product=rep_prod.
+                              get_product_by_id(product_id),
+                              seller=rep_seller.
+                              get_seller(seller_id))
+                    * session_products[f'{product_id} {seller_id}'])
+                #  сумма товаров в корзине,
+                #  с помощью цикла берутся product_id, seller_id, count
+                #  из сессии
+            total_amount = sum(cart_sum)
+        else:
+            total_amount = 0
         return total_amount
