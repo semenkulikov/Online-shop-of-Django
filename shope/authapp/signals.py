fcd843e4259cb0010abbcdca425080b504931c2d
Alexander Barshev <drivetime@bk.ru>
2023-06-27 19:37:10 +0300

    add roles

---
+++ shope/authapp/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import Group
+
+from authapp.models import User
+
+
+@receiver(post_save, sender=User)
+def add_to_sellers_group(sender, instance, created, **kwargs):
+    if instance.is_staff:
+        sellers_group = Group.objects.get(name='Продавцы')
+        sellers_group.user_set.add(instance)
