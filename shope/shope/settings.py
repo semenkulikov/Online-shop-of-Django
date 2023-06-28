157b8957dda7a08d1da37189e4c1b71f5830ab44
Sergei Nozovtsev <glenmetthews@gmail.com>
2023-06-28 16:39:26 +0300

    added memcached in docker-compose.yaml

--- shope/shope/settings.py
+++ shope/shope/settings.py
@@ -146,7 +146,7 @@ else:
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        "LOCATION": "127.0.0.1:11211",
+        "LOCATION": "cache:11211",
     }
 }
 
