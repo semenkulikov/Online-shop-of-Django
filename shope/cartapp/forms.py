b75ca26a979328a514ac07332915443602bdf234
arsen_iamaleev <iiforex@mail.ru>
2023-06-30 13:20:08 +0300

    Fix Bugs in CART APP
    Took 1 minute

--- shope/cartapp/forms.py
+++ shope/cartapp/forms.py
@@ -5,7 +5,7 @@ class InputAmountForm(forms.Form):
     """
     Форма, используемая при работе с корзиной
     """
-    count = forms.IntegerField(required=False, min_value=1, max_value=10)
+    count = forms.IntegerField(required=False, min_value=1, max_value=100)
     product_id = forms.IntegerField(required=True)
     seller_id = forms.IntegerField(required=True)
 
